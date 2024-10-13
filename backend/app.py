6f9e182d8a256418929bf58f8a58645229c57644
Vishesh Pandey <visheshpandey24@gmail.com>
2024-10-13 22:01:47 +0000

    basic updates

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,14 @@
 # app.py
 from flask import Flask, request, jsonify
-import numpy as np
-from sklearn.linear_model import LinearRegression
 from flask_cors import CORS
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
+
 CORS(app)
 
-# Load or train your ML model (example: linear regression)
-model = LinearRegression()
-
-# Dummy data for training (X: Features, y: Target)
-X = np.array([[1, 2], [2, 3], [3, 4]])
-y = np.array([3, 5, 7])
-
-# Train the model
-model.fit(X, y)
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get the input data from the client
-        input_data = request.json['data']
-        input_array = np.array(input_data)
-
-        # Use the model to make a prediction
-        prediction = model.predict([input_array])
-
-        # Return the result as JSON
-        return jsonify({'prediction': prediction.tolist()})
-    
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    
 @app.route('/bestfitline', methods=['POST'])
 def bestfitline():
     '''
@@ -50,17 +25,28 @@ def bestfitline():
     try:
         # Get the input data from the client
         input_data = request.json['data']
-        X = np.array([x[0] for x in input_data]).reshape(-1, 1)
-        y = np.array([x[1] for x in input_data])
+        
+        x_list = []
+
+        for i in input_data : 
+            x_list.append(i[0])
+
+        y_list = [] 
+
+        for j in input_data : 
+            y_list.append(j[1])
+
+        X = np.array(x_list).reshape(-1, 1)
+        y = np.array(y_list)
 
         # Create a new linear regression model
-        new_model = LinearRegression()
+        model = LinearRegression()
 
         # Train the model
-        new_model.fit(X, y)
+        model.fit(X, y)
 
         # Return the result as JSON
-        return jsonify({'coeff': new_model.coef_.tolist(), 'intercept': new_model.intercept_.tolist()})
+        return jsonify({'coeff': model.coef_.tolist(), 'intercept': model.intercept_.tolist()})
     
     except Exception as e:
         return jsonify({'error': str(e)})
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,68 +8,80 @@ function App() {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "50vh",
-          border: "1px solid black",
-        }}
-        onClick={(e) => {
-          const point = {
-            x: e.clientX,
-            y: e.clientY,
-          };
-          const currentPoints = [...points, point];
-          setPoints((prevPoints) => [...prevPoints, point]);
-          if (currentPoints.length < 2) return;
-          // if running program on github codespaces then make port public from below terminal ( port ) option and use the link given there that looks like this :
-          // https://supreme-garbanzo-pvxpxj7jpf996g-5000.app.github.dev/bestfitline
-          fetch("http://127.0.0.1:5000/bestfitline", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              data: currentPoints.map((point) => [point.x, point.y]),
-            }),
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              console.log("Data : ", data);
-              setCoeff(data.coeff);
-              setIntercept(data.intercept);
-            })
-            .catch((err) => console.error(err));
-        }}
-      >
-        <svg
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          {points.map((point, index) => (
-            <circle key={index} cx={point.x} cy={point.y} r={5} fill="black" />
-          ))}
-          {!coeff.length ? null : (
-            <line
-              x1={0}
-              y1={coeff[0] * 0 + intercept}
-              x2={500}
-              y2={coeff[0] * 500 + intercept}
-              style={{ stroke: "red", strokeWidth: 2 }}
-            />
-          )}
-        </svg>
-      </div>
-      <div>
-        <p>Click on the graph to add points</p>
-        <p> coeff: {coeff} </p>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <div style={{ width: "50%" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50vh",
+              border: "1px solid black",
+            }}
+            onClick={(e) => {
+              const point = {
+                x: e.clientX,
+                y: e.clientY,
+              };
+              const currentPoints = [...points, point];
+              setPoints((prevPoints) => [...prevPoints, point]);
+              if (currentPoints.length < 2) return;
+              // if running program on github codespaces then make port public from below terminal ( port ) option and use the link given there that looks like this :
+              // https://supreme-garbanzo-pvxpxj7jpf996g-5000.app.github.dev/bestfitline
+              fetch(
+                "https://supreme-garbanzo-pvxpxj7jpf996g-5000.app.github.dev/bestfitline",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    data: currentPoints.map((point) => [point.x, point.y]),
+                  }),
+                }
+              )
+                .then((res) => res.json())
+                .then((data) => {
+                  setCoeff(data.coeff);
+                  setIntercept(data.intercept);
+                })
+                .catch((err) => console.error(err));
+            }}
+          >
+            <svg
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              
+
+
+              {points.map((point, index) => (
+                <circle
+                  key={index}
+                  cx={point.x}
+                  cy={point.y}
+                  r={5}
+                  fill="white"
+                />
+              ))}
+              {!coeff.length ? null : (
+                <line
+                  x1={0}
+                  y1={coeff[0] * 0 + intercept}
+                  x2={1400}
+                  y2={coeff[0] * 1400 + intercept}
+                  style={{ stroke: "yellow", strokeWidth: 2 }}
+                />
+              )}
+            </svg>
+          </div>
+        </div>
+        <div style={{ width: "50%", backgroundColor: "yellow" }}></div>
       </div>
     </>
   );
